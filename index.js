7af9e491fc210f6568f42d5b95470e710304d68d
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-07-01 03:00:18 +0530

    port 80

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require('express')
 const app = express()
-const port = 5500
+const port = 80
 const router = express.Router();
 
 // additional modules
